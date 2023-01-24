40f3fd10eed49cba6bb1d59543766ed4ee6a294c
rale11420 <44586074+rale11420@users.noreply.github.com>
2023-01-24 16:55:43 +0100

    ERC20 sFil contract created

--- contracts/sFil.sol
+++ contracts/sFil.sol
@@ -1,2 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract sFil is ERC20 {
+    constructor() ERC20("Staked FIL", "stFil") {}
+
+    function donate() external payable {
+        _mint(msg.sender, msg.value);
+    }
+
+    function withdraw() external {
+        address temp = msg.sender;
+        uint256 amount = balanceOf(temp);
+
+        (bool success, ) = temp.call{value: amount}("");
+        require(success, "Failed to send FIL");
+
+        _burn(temp, amount);
+    }
+}
