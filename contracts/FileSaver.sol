2cc52dabc7b9529fe93f0e42a191b159af1f20c4
ivana-mvpw <ivana.vasic@mvpworkshop.co>
2023-01-25 16:54:37 +0100

    cmt

--- contracts/FileSaver.sol
+++ contracts/FileSaver.sol
@@ -1,4 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-contract FileSaver {}
+import "./sFil.sol";
+
+error InsufficientFunds();
+
+contract FileSaver {
+    mapping(address => mapping(uint64 => uint256)) public balances;
+    sFil internal immutable sFilToken;
+
+    constructor() {
+        sFilToken = new sFil();
+    }
+
+    function deposit(uint64 dealID) external payable {
+        if (msg.value <= 0) revert InsufficientFunds();
+
+        sFilToken.mint(msg.sender, msg.value);
+
+        balances[msg.sender][dealID] += msg.value;
+    }
+
+    function withdraw(uint64 dealID, uint256 amount) external {
+        if (balances[msg.sender][dealID] < amount) revert InsufficientFunds();
+
+        balances[msg.sender][dealID] -= amount;
+
+        sFilToken.burn(msg.sender, amount);
+
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed.");
+    }
+}
--- contracts/sFil.sol
+++ contracts/sFil.sol
@@ -2,21 +2,16 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract sFil is ERC20 {
-    constructor() ERC20("Staked FIL", "stFil") {}
+contract sFil is ERC20, Ownable {
+    constructor() ERC20("Staked FIL", "SFIL") {}
 
-    function donate() external payable {
-        _mint(msg.sender, msg.value);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    function withdraw() external {
-        address temp = msg.sender;
-        uint256 amount = balanceOf(temp);
-
-        (bool success, ) = temp.call{value: amount}("");
-        require(success, "Failed to send FIL");
-
-        _burn(temp, amount);
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
     }
 }
