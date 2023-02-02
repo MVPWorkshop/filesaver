becd83b1fd525cac07591759580c0c690f2ed89f
rale11420 <44586074+rale11420@users.noreply.github.com>
2023-02-02 08:15:47 +0100

    Create BountySC

---
+++ contracts/BountySC.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+contract BountySC {
+    
+    function reserveSpot(uint256 dealID) external {}
+
+    function createDeal() external {}
+
+    function getDealInfo(uint256 dealID) external {}
+
+    function claimBounty(uint256 dealID) external {}
+
+}
