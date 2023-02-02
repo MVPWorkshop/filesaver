737c867840b20fa2a2cc8ea2da5dcc10f6d9599b
Uros Kukic <>
2023-02-02 10:28:36 +0100

    Add offer publishing and application socket communication

--- clients/filesaver-client.js
+++ clients/filesaver-client.js
@@ -1,25 +1,45 @@
+const fs = require('fs');
 const app = require('express')();
+const multiparty = require('multiparty');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-let usernames = {};
+let addresses = {};
 let ids = {};
 
 // API Calls
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/store', (req, res) => {
-    io.emit('new offer');
-    res.send('done');
+    var form = new multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        console.log('Received storage request');
+
+        const offer_price = fields.value[0];
+        const offer_length = fields.length[0];
+        const offer_size = files.file[0].size;
+        // TODO determine file CID and send correct value
+        const offer_cid = Math.trunc(Math.random() * 10000000000);
+
+        const payload = { offer_price, offer_length, offer_size, offer_cid };
+        io.emit('fs-offer', payload);
+
+        res.sendStatus(200);
+    });
 })
 
-function addUserData(userID, username, socket) {
-    usernames[userID] = username;
-    ids[username] = userID;
+app.get('/filestatus', (req, res) => {
+    let payload = {};
+
+    res.status(200).send(JSON.stringify(payload));
+})
+
+function addUserData(sp_id, sp_address, socket) {
+    addresses[sp_id] = sp_address;
+    ids[sp_address] = sp_id;
 }
 
 // Socket calls
@@ -27,34 +47,39 @@ function addUserData(userID, username, socket) {
 io.on('connection', async (socket) => {
     console.log(`User ${socket.client.id} attempting to connect...`);
 
-    socket.on('set_username', msg => {
-        console.log(`User ${socket.client.id} set their username to ${msg}`);
+    socket.on('sp-set-address', msg => {
+        console.log(`User ${socket.client.id} set their address to ${msg}`);
 
         addUserData(socket.client.id, msg, socket);
 
         socket.join(socket.client.id);
 
-        io.to(ids[msg]).emit('confirm_username', usernames[socket.client.id]);
+        io.to(ids[msg]).emit('fs-confirm-address', addresses[socket.client.id]);
     })
 
-    socket.on('sp-claim', data => {
-        let { sp_address } = data;
+    socket.on('sp-apply', data => {
+        let { address: sp_address, offer_cid } = data;
+        console.log(`User ${sp_address} is applying for offer ${offer_cid}`);
+
+        createDealWithStorageProvider(socket, offer_cid, sp_address)
     });
 
+    /* Reference for direct messages
     socket.on('chat message', msg => {
-        if (!usernames[socket.client.id]) {
+        if (!addresses[socket.client.id]) {
             console.log('User not found, rejecting message');
-            io.to(socket.client.id).emit('nousername', {});
+            io.to(socket.client.id).emit('no_username');
             return;
         }
 
-        console.log(`${usernames[socket.client.id]} sending message ${msg}`);
+        console.log(`${addresses[socket.client.id]} sending message ${msg}`);
 
-        io.emit('chat message', { value: msg, username: usernames[socket.client.id] });
+        io.emit('chat message', { value: msg, username: addresses[socket.client.id] });
     });
+    */
 
     socket.on('disconnect', () => {
-        console.debug(`User ${usernames[socket.client.id]} disconnected`);
+        console.debug(`User ${addresses[socket.client.id]} disconnected`);
     });
 
 });
--- clients/service-provider-client.js
+++ clients/service-provider-client.js
@@ -1,34 +1,51 @@
+require('dotenv').config();
 const app = require('express')();
 const { io } = require("socket.io-client");
 const socket = io.connect('http://localhost:3000', { reconnect: true });
 
 const http = require('http').Server(app);
 
-var args = process.argv.slice(2);
+const address = process.env.SP_USERNAME;
+const port = process.env.SP_PORT;
 
-const username = args[0];
-const port = args[1];
+// Helper functions 
+function checkOfferCriteria(offer_price, offer_length, offer_size) {
+    // TODO Add checking service provider criteria
+    return true;
+}
 
+function applyForOffer(socket, offer_cid) {
+    console.log(`Applying for offer ${offer_cid}`);
+    socket.emit('sp-apply', { address, offer_cid });
+}
+
+// Socket events
 socket.on('fs-offer', function (data) {
     console.log('======= Detected new offer =======');
     const { offer_price, offer_length, offer_size, offer_cid } = data;
-    console.log(`\tOffer CID: ${offer_cid}`);
-    console.log(`\tOffer price: ${offer_price}`);
-    console.log(`\tOffer size:  ${offer_size}`);
-    console.log(`\tOffer length: ${offer_length}`);
+    console.log(`Offer ${offer_cid} price: ${offer_price}`);
+    console.log(`Offer ${offer_cid} size:  ${offer_size}`);
+    console.log(`Offer ${offer_cid} length: ${offer_length}`);
 
+    const offerCriteriaPassed = checkOfferCriteria(offer_price, offer_length, offer_size);
+    if (!offerCriteriaPassed) {
+        console.log(`Offer ${offer_cid} does not meet node criteria, ignoring offer.`);
+        return;
+    }
+
+    applyForOffer(socket, offer_cid);
 })
 
 socket.on('connect', function (connection) {
-    console.log('Setting username...');
-    socket.emit('set_username', username);
+    console.log('Setting address...');
+    socket.emit('sp-set-address', address);
 });
 
-socket.on('confirm_username', function (server_username) {
-    if (server_username === username) {
-        console.log(`Successfully connected to server and set username!`);
+socket.on('fs-confirm-address', function (confirmation_address) {
+    if (confirmation_address === address) {
+        console.log(`Successfully connected to server and set address!`);
     } else {
-        throw Error(`Server connection failed! Received username ${server_username}`);
+        throw Error(`Server connection failed! Received address ${confirmation_address}`);
     }
 })
 
