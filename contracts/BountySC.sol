c42765069d6e182080f7e739ad444096a2f8b1a6
rale11420 <44586074+rale11420@users.noreply.github.com>
2023-02-03 09:56:34 +0100

    reserveSpot, createDeal

--- contracts/BountySC.sol
+++ contracts/BountySC.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-contract BountySC {
-    
-    function reserveSpot(uint256 dealID) external {}
+import {MarketAPI} from "@zondax/filecoin-solidity/contracts/v0.8/MarketAPI.sol";
+import {MarketTypes} from "@zondax/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
+import "./sFil.sol";
 
-    function createDeal() external {}
+contract BountySC {
+    event DealCreated(address client);
+
+    function reserveSpot(bytes32 cid) external {}
+
+    function createDeal(MarketTypes.PublishStorageDealsParams memory params) external payable {
+        MarketAPI.addBalance(abi.encodePacked(msg.sender));
+        MarketAPI.publishStorageDeals(params);
+
+        //get dealID
+
+        emit DealCreated(MarketAPI.getDealClient(dealID));
+    }
 
     function getDealInfo(uint256 dealID) external {}
 
     function claimBounty(uint256 dealID) external {}
-
 }
+
