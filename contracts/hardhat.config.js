084f97c03306064a448af0072cee1eb86e3aa5ee
bojinovic <milos.bojinovic@mvpworkshop.co>
2023-02-06 13:52:43 +0100

    Contracts - cleaning up subdir

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 require("hardhat-deploy-ethers")
-require("./tasks")
+// require("./tasks")
 require("dotenv").config()
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
